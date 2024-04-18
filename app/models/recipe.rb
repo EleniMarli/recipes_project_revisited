3032d0bc8f77e1809758e1c668e956ee541525a5
Eleni Marli <elenimarli@yahoo.gr>
2024-04-18 11:34:27 +0200

    Write validations for Recipe class

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :ingredients
+  has_many :ingredients, dependent: :destroy
+
+  validates :name, :portions, :instructions, :time_in_min, :difficulty, :instructions, presence: true
+  validates :portions, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
+  validates :time_in_min, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5000 }
+  validates :difficulty, inclusion: { in: %w[easy medium difficult] }
 end
