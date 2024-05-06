c11c4f966c002de10221b793f661b328d6b91d11
Eleni Marli <elenimarli@yahoo.gr>
2024-05-06 13:53:47 +0200

    Work on creating Ingredients while creating Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,36 +19,42 @@ class RecipesController < ApplicationController
       @recipe.list_of_ingredients.split("\n").reject{ |str| str == "" }.map(&:strip).each do |str|
 
         # START WITH AMOUNT
-
-        # num_str_comma = str.scan(/\d+,\d+/)
         str.gsub(/\d+,\d+/) { |match| match.gsub(',', '.') }
 
         amount = str.scan(/[-+]?\d*\.?\d+/).first
         unless amount.nil?
-          amount = amount.to_f
           str.gsub!(amount, " ")
+          amount = amount.to_f
         end
 
         # THEN METRIC UNIT
-        metric_unit = METRIC_UNITS.find { |word| str.include?(word) }
-        # metric_unit = "unit(s)" if metric_unit.nil?
+        metric_unit = Ingredient.metric_units.find { |word| str.match?(/\b#{word.downcase}\b/i) }
+
+        #"250g penne pasta\n400 gr canned diced tomatoes\n1 onion, finely chopped\n2 cloves garlic, minced\n200 oz spinach leaves\n100 mL feta cheese, crumbled\n2 liter olive oil\n1 bunch dried oregano\nsalt and pepper to taste"
+
         unless metric_unit.nil?
+          str.gsub!(/\b#{metric_unit.downcase}\b/i, "")
           metric_unit.downcase!
-          str.gsub!(metric_unit, "")
+        end
 
         # REST IS NAME
-        name = str.gsub(/\s+/, " ")
+        name = str.gsub(/\s+/, " ").strip
 
-        ingredient = Ingredient.new(name: name, amount: amount, metric_unit: metric_unit, recipe_id: @recipe.id)
+        ingredient = Ingredient.new(name: name, amount: amount, metric_unit: metric_unit, recipe_id: nil)
         ingr_instances_list << ingredient if ingredient.valid?
         if !ingredient.valid?
-          flash.now[:notice] = "One of the ingredients is not valid."
+          flash.now[:notice] = "One of the ingredients is not valid. Use a more standardised format (e.g. '400 gr pasta')"
           render :new, status: :unprocessable_entity
+          return
         end
       end
 
       if @recipe.save
-        ingr_instances_list.each { |ingr| ingr.save }
+        ingr_instances_list.each do |ingr|
+          ingr.recipe_id = @recipe.id
+          ingr.save
+        end
+        format_list_of_ingredients(@recipe)
         redirect_to recipe_path(@recipe), notice: "Recipe saved succesfully"
       else
         render :new, status: :unprocessable_entity
@@ -79,6 +85,19 @@ class RecipesController < ApplicationController
   end
 
   private
+
+  def format_list_of_ingredients(recipe)
+    new_list_of_ingredients = recipe.ingredients.map do |ingr|
+      if ingr.amount && ingr.metric_unit
+        "#{ingr.amount} #{ingr.metric_unit} #{ingr.name}"
+      elsif ingr.amount
+        "#{ingr.amount} #{ingr.name}"
+      else
+        ingr.name.to_s
+      end
+    end.join("\n")
+    recipe.update(list_of_ingredients: new_list_of_ingredients)
+  end
 
   def set_recipe
     @recipe = Recipe.find(params[:id])
